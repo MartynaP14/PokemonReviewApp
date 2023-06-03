197eb484d68330e671a719304f96a73530911915
Martyna Martyna <martynapacula@gmail.com>
2023-06-03 14:05:40 +0100

    initial pokemon client build

---
+++ client/Client/PokemonClient.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using pokemonAppClient.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemonAppClient.Client
+{
+    public class PokemonClient : IPokemonClient
+    {
+
+        private HttpClient _httpClient;
+        private readonly string _url = "https://localhost:7107";
+
+
+
+        public PokemonClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(_url);
+        }
+
+
+        public async Task<HttpResponseMessage> GetCountriesAsync()
+        {
+            return await _httpClient.GetAsync("api/country");
+           
+        } 
+
+
+
+        //HttpClient client = new HttpClient();
+
+        //client.BaseAddress = new Uri("https://localhost:7107");
+
+
+        //HttpResponseMessage response = await client.GetAsync("api/country");
+        //var responseBody = await response.Content.ReadAsStringAsync();
+        //Console.WriteLine(response);
+        //Console.WriteLine(responseBody);
+    }
+}
